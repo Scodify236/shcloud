32ac045e9a40ae34ed3f8229644f4a8dc9485626
Scodify236 <scoder120@gmail.com>
2024-12-11 21:05:21 +0530

    base code update bugs fixes...

---
+++ src/modules/setAudioStreams.ts
@@ -0,0 +1,67 @@
+import { audio, bitrateSelector, playButton } from "../lib/dom";
+import { getSaved, store } from "../lib/store";
+import { notify } from "../lib/utils";
+
+export function setAudioStreams(audioStreams: {
+  codec: string,
+  url: string,
+  quality: string,
+  bitrate: string,
+  contentLength: number,
+  mimeType: string
+}[],
+  isMusic = false,
+  isLive = false) {
+
+  const preferedCodec = store.player.codec;
+  const noOfBitrates = audioStreams.length;
+  let index = -1;
+
+  if (!noOfBitrates) {
+    notify(
+      isLive ?
+        'Turn on HLS to listen to LiveStreams!' :
+        'No Audio Streams Found.'
+    );
+    playButton.classList.replace(playButton.className, 'ri-stop-circle-fill');
+    return;
+  }
+
+  function proxyHandler(url: string) {
+    
+    if (url.startsWith('https://ymd.dlod.link/?u='))
+      url = url.slice(25);
+    
+    const useProxy = isMusic || getSaved('enforceProxy');
+    const oldUrl = new URL(url);
+    const origin = oldUrl.origin;
+    const isInvidiousResponse = store.api.invidious.includes(origin);
+    const proxy = isInvidiousResponse ? origin : store.player.proxy;
+      
+    if (url.startsWith('https://r'))
+      return url.replace(origin, store.player.proxy) + '&host=' + origin.slice(8);
+
+    return url.replace(origin, useProxy ? proxy :
+        'https://redirector.googlevideo.com'
+    );
+
+  }
+
+  bitrateSelector.innerHTML = '';
+  audioStreams.forEach((_, i: number) => {
+    const codec = _.codec === 'opus' ? 'opus' : 'aac';
+    const size = (_.contentLength / (1024 * 1024)).toFixed(2) + ' MB';
+
+    // add to DOM
+    bitrateSelector.add(new Option(`${_.quality} ${codec} - ${size}`, proxyHandler(_.url)));
+
+    (<HTMLOptionElement>bitrateSelector?.lastElementChild).dataset.type = _.mimeType;
+    // find preferred bitrate
+    const codecPref = preferedCodec ? codec === preferedCodec : true;
+    const hqPref = store.player.hq ? noOfBitrates : 0;
+    if (codecPref && index < hqPref) index = i;
+  });
+
+  bitrateSelector.selectedIndex = index;
+  audio.src = bitrateSelector.value;
+}
